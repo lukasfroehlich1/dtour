a9888875e46d22dc0636ae299cbc6b6769e0a547
Tiange Wu <origami7795@gmail.com>
2015-10-03 16:43:58 -0700

    Added web app basics

--- server.js
+++ server.js
@@ -1,77 +1,16 @@
-var polyline = require('polyline');
-var geolib = require('geolib');
-var GoogleMapsAPI = require('googlemaps');
+var express = require('express');
+var app = express();
 
-var yelp = require("yelp").createClient({
-  consumer_key: "BRao3_-71k3UGVBQAOCHAg", 
-  consumer_secret: "v57ivrvRCFpmjyoHrAVvyNMsBK8",
-  token: "G77q0HeRY32ONNVC2pQPh8fMMSYdVrAd",
-  token_secret: "klbPKxqjk1a_a3My-IoE7rkl_qE"
+app.set('view engine', 'jade');
+app.use(express.static(__dirname + '/static'));
+
+app.get('/', function(req, res) {
+    res.render('home', { title: 'Hey', message: 'Hello there!'});
 });
 
-var publicConfig = {
-  key: 'AIzaSyAl1AUh9oQiTgNrNfeLW1RIOLZyzbKXSjA',
-  stagger_time:       1000, // for elevationPath
-  encode_polylines:   false,
-  secure:             true, // use https
-};
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
 
-var gmAPI = new GoogleMapsAPI(publicConfig);
-
-var params = {
-    origin: "22 fairmount st, 94131",
-    destination: "572 boysen st, 93405",
-    mode: "driving"
-}
-trip_suggestions = function() {
-
-    gmAPI.directions(params, function(err, results){
-        var steps = results["routes"][0]["legs"][0]["steps"];
-        var dist = results["routes"][0]["legs"][0]["distance"]["value"];
-        var coords = calculate_middle(steps, dist);
-
-        var input = {term: "food",radius_filter: 10 , ll: coords[0] + ',' + coords[1]};
-
-        yelp.search(input, function(error, data) {
-            console.log(error);
-            console.log(data["businesses"][0]);
-        });
-    });
-
-
-}
-
-gmAPI.directions(params, function(err, results){
-    var steps = results["routes"][0]["legs"][0]["steps"];
-    var dist = results["routes"][0]["legs"][0]["distance"]["value"];
-    var coords = calculate_middle(steps, dist);
-
-    var input = {term: "food",radius_filter: 10 , ll: coords[0] + ',' + coords[1]};
-
-    yelp.search(input, function(error, data) {
-        console.log(error);
-        console.log(data["businesses"][0]);
-    });
+    console.log('Example log listening at http://%s:%s', host, port);
 });
-
-calculate_middle = function(steps, dist){
-    var cur_dist = 0;
-    var middle = dist/2;
-    for (i=0; i<steps.length; i++) {
-        cur_dist += steps[i]["distance"]["value"];
-        if (cur_dist > middle) {
-            cur_dist -= steps[i]["distance"]["value"];
-            var middle_step = steps[i]
-            var line_points = polyline.decode(middle_step['polyline']['points']);
-            for (j=0; j<line_points.length-1; j++) {
-                var start = {latitude: line_points[j][0] ,longitude: line_points[j][1]};
-                var end = {latitude: line_points[j+1][0],longitude: line_points[j+1][1]};
-                cur_dist += geolib.getDistance(start,end);
-                if (cur_dist > middle) {
-                    return line_points[j];
-                }
-            }
-        }
-    }
-    return -1;
-}
